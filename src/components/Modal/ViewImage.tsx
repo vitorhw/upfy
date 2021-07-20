c7944f921cbbb71b8b6e71f4d5b5cd2df67a3155
welzel855 <welzel855@hotmail.com>
2021-07-20 11:53:11 -0300

    first commit

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -19,5 +19,30 @@ export function ModalViewImage({
   onClose,
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
-  // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} isCentered size="4xl">
+      <ModalOverlay />
+      <ModalContent
+        mx="auto"
+        w="auto"
+        h="auto"
+        maxW='900px'
+        maxH='600px'
+      >
+        <ModalBody p="0">
+          <Image 
+            src={imgUrl}
+            maxW='900px'
+            maxH='600px'
+          /> 
+
+        </ModalBody>
+        <ModalFooter bg="pGray.800" h="2rem" px="20px" borderBottomRadius="5px">
+          <Link href={imgUrl} isExternal fontSize="1rem" mr="auto">
+            Abrir original
+          </Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,29 @@ import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
+type Image = {
+  title: string,
+  description: string,
+  url: string,
+  ts: number,
+  id: string
+}
+
+type GetImagesResponse = {
+  after: string,
+  data: Image[]
+}
+
 export default function Home(): JSX.Element {
+  async function getImages({ pageParam = null }): Promise<GetImagesResponse> {
+    const { data } = await api('api/images', {
+      params: {
+        after: pageParam
+      }
+    });
+    return data
+  }
+
   const {
     data,
     isLoading,
@@ -18,18 +40,26 @@ export default function Home(): JSX.Element {
     hasNextPage,
   } = useInfiniteQuery(
     'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
+    getImages,
+    {
+      getNextPageParam: lastPage => lastPage?.after || null
+    }
   );
 
   const formattedData = useMemo(() => {
-    // TODO FORMAT AND FLAT DATA ARRAY
+    const formatted = data?.pages.flatMap(imageData => {
+      return imageData.data.flat();
+    })
+    return formatted;
   }, [data]);
 
-  // TODO RENDER LOADING SCREEN
+  if (isLoading && !isError) {
+    return <Loading />
+  }
 
-  // TODO RENDER ERROR SCREEN
+  if (!isLoading && isError) {
+    return <Error />
+  }
 
   return (
     <>
@@ -37,7 +67,15 @@ export default function Home(): JSX.Element {
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        {hasNextPage && (
+          <Button 
+            onClick={() => fetchNextPage()}
+            disabled={isFetchingNextPage}
+            mt="6"
+          >
+            {isFetchingNextPage ? 'Carregando...' : 'Carregar mais'}
+          </Button>
+        )}
       </Box>
     </>
   );
